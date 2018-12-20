eb65488de971dcf8e4cc5e595446bb918a5fde04
Stephen Grosjean <stephen-gm@hotmail.com>
2018-12-21 00:14:32 +0100

    Finished rhythm particles and added KoreoGraphy plugin

---
+++ Assets/EventSubscribe.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class EventSubscribe : MonoBehaviour {
+    [SerializeField] private string eventID;
+    [SerializeField] private RhythmSpawner rhythmSpawnerComponent;
+
+
+    void FireEventDebugLog(KoreographyEvent koreoEvent)
+    {
+        rhythmSpawnerComponent.Spawn();
+
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+
+
+        Koreographer.Instance.RegisterForEvents(eventID, FireEventDebugLog);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    [SerializeField] private float speed, step;
+
+
+    private bool pongMode;
+    private Transform destinationTransform;
+    private List<Transform> poses;
+    private bool arrived;
+    private int i;
+
+    private Vector2 LerpPos;
+    private float moveTime;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere((Vector3)LerpPos, 0.1f);
+    }
+
+    // Use this for initialization
+    void Start () {
+        float angle = transform.localEulerAngles.z;
+        Vector2 dir = new Vector2(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle));
+
+        GetComponent<Rigidbody2D>().velocity = dir * speed;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (pongMode)
+        {
+            moveTime += Time.deltaTime * step;
+            transform.position = Vector2.Lerp(LerpPos, destinationTransform.position,moveTime);
+        }
+    }
+
+    public void BulletPong(List<Transform> pos)
+    {
+        poses = pos;
+        LerpPos = transform.position;
+        destinationTransform = poses[0];
+        pongMode = true;
+    }
+
+    public void Next(GameObject obj)
+    {
+        moveTime = 0;
+        obj.GetComponent<RhythmController>().DestroySequence();
+        LerpPos = poses[i].position;
+        i++;
+
+        if (i == poses.Count)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            destinationTransform = poses[i];
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "RhythmParticle")
+        {
+            if(collision.gameObject == destinationTransform.gameObject)
+            {
+                    Next(collision.gameObject);
+            }
+        }
+    }
+}
